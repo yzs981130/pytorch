02065b84fc6cb0447e31b11b47a0b1db6f73b697
Horace He <chilli@fb.com>
2023-05-12 06:17:42 +0000

    Communication reordering pass
    ghstack-source-id: d5373a0c7c5924ff5a91901f700621340e3b39c4
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/100778

--- torch/_inductor/scheduler.py
+++ torch/_inductor/scheduler.py
@@ -16,7 +16,7 @@ from torch._dynamo.utils import dynamo_timed
 from . import config, dependencies, ir, metrics
 from .dependencies import StarDep, WeakDep
 from .sizevars import SimplifyIndexing
-from .utils import cache_on_self, cmp, free_symbol_has, has_triton
+from .utils import cache_on_self, cmp, free_symbol_has, has_triton, is_local
 from .virtualized import V
 
 log = logging.getLogger(__name__)
@@ -55,6 +55,7 @@ class BaseSchedulerNode:
         self.node: ir.Buffer = node
         self.users: Optional[List[NodeUser]] = None
         self.inverse_users: List[BaseSchedulerNode] = []
+        self.additional_dependencies = set()
         self.set_read_writes(node.get_read_writes())
         self.recursive_predecessors: Optional[Set[str]] = None
         self.min_order: Optional[int] = None
@@ -132,6 +133,7 @@ class BaseSchedulerNode:
         }
 
     def prune_deps(self):
+        old = set(self.unmet_dependencies)
         self.unmet_dependencies = {
             dep
             for dep in self.unmet_dependencies
@@ -663,12 +665,25 @@ class Scheduler:
 
         metrics.ir_nodes_pre_fusion += len(self.nodes)
         V.debug.ir_pre_fusion(self.nodes)
+        from .debug import draw_buffers
+
+
+        comm_nodes = [n for n in self.nodes if isinstance(n.node, ir.CollectiveKernel)]
+        for i in range(1, len(comm_nodes)):
+            comm_nodes[i].unmet_dependencies.add(StarDep(comm_nodes[i - 1].get_name()))
+        self.compute_predecessors()
         self.num_orig_nodes = len(self.nodes)
         self.name_to_fused_node = {n.get_name(): n for n in self.nodes}
+
         self.fuse_nodes()
         self.compute_last_usage()
         V.debug.ir_post_fusion(self.nodes)
         V.debug.graph_diagram(self.nodes)
+        comm_nodes = [n for n in self.nodes if isinstance(n.node, ir.CollectiveKernel)]
+        for i in range(1, len(comm_nodes)):
+            comm_nodes[i].unmet_dependencies.add(StarDep(comm_nodes[i - 1].get_name()))
+        # if is_local():
+        #     draw_buffers(self.nodes, fname='post_fused.svg')
         self.debug_draw_graph()
 
         # used during codegen:
