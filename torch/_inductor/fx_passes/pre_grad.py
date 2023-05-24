92a8b524472b4c0ace0e5245f5500d62ccce8a04
leslie-fang-intel <leslie.fang@intel.com>
2023-05-24 12:32:57 +0800

    Enable conv binary for inductor
    ghstack-source-id: a6de8ed54874bdf423d5c9df03c8be01a06be407
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/97431

--- torch/_inductor/fx_passes/pre_grad.py
+++ torch/_inductor/fx_passes/pre_grad.py
@@ -19,6 +19,7 @@ from ..fx_utils import matches_module_function_pattern
 from ..mkldnn import mkldnn_fuse_fx
 from ..pattern_matcher import init_once_fakemode, PatternMatcherPass
 from ..utils import is_cpu_device
+from .quantization_utils import fuse_quantization
 
 log = logging.getLogger(__name__)
 
@@ -63,7 +64,7 @@ def pre_grad_passes(gm, example_inputs):
         gm = fuse_fx(gm, example_inputs)
         for pattern_matcher_pass in pattern_matcher_passes:
             pattern_matcher_pass.apply(gm.graph)
-        gm = overrides.fuse_quantization(gm, example_inputs)
+        gm = fuse_quantization(gm, example_inputs)
 
     gm.graph.lint()
     gm.recompile()
