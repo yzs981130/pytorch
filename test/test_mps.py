9be5bed34223781e5bd693e00c6d6b152ef8deee
Sun, Jiayi <jiayi.sun@intel.com>
2023-06-09 22:10:17 +0800

    add Half support for layer_norm on CPU
    ghstack-source-id: 91d7ea09a87d6c4623bc0422c62e0c1c1a71bf0e
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/99590

--- test/test_mps.py
+++ test/test_mps.py
@@ -125,6 +125,9 @@ def mps_ops_grad_modifier(ops):
 
         # round not working properly for float16
         'round': [torch.float16],
+
+        # Mixed datatype is recommended for layer_norm float16 on CPU
+        'nn.functional.layer_norm': [torch.float16],
     }
 
     MACOS_12_3_XFAILLIST_GRAD = {
@@ -10408,6 +10411,8 @@ class TestConsistency(TestCaseMPS):
         'nn.functional.triplet_margin_loss',
         'nn.functional.triplet_margin_with_distance_loss',
         'round', 'xlogy',
+        'native_layer_norm',
+        'nn.functional.layer_norm',
 
         # for macOS 12
         'masked.normalize', 'masked.sum', 'masked.var',
