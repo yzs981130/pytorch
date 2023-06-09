f0ea98a2cbd95992c0b27956dc858d8d5ea5393a
Yukio Siraichi <yukio.siraichi@gmail.com>
2023-06-09 18:14:49 -0300

    Value range refinement using multi-variate expressions.
    ghstack-source-id: 3180a23da5be5ff2b6662c8a8494b99dbf28cc40
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/97964

--- torch/fx/experimental/symbolic_shapes.py
+++ torch/fx/experimental/symbolic_shapes.py
@@ -3719,10 +3719,6 @@ Failed inputs:
             ):
                 continue
 
-            # Use only univariate functions.
-            if len(expr.rhs.free_symbols) > 0:
-                continue
-
             # Update the value range of the left-hand side, if the
             # right-hand side provides a better range.
             symbol = expr.lhs
