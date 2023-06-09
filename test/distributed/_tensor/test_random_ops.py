9dbe2ef60c42d4b68a8b3fc521acb2858655fec0
Xilun Wu <12968408+XilunWu@users.noreply.github.com>
2023-06-09 16:58:51 +0000

    [DTensor][Random] Introduce CudaRNGStatesTracker to maintain the default and model parallel seeds
    ghstack-source-id: 17be53452cae8f598a9fac5320a2b1bea1f3b443
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/103235

--- test/distributed/_tensor/test_random_ops.py
+++ test/distributed/_tensor/test_random_ops.py
@@ -45,9 +45,6 @@ class DistTensorRandomInitTest(DTensorTestBase):
             dtensor = init_op(dtensor, *args, **kwargs)
             self.assertEqual(local_tensor_clone, dtensor.to_local())
         else:
-            # initialize rng state
-            manual_seed(1234, device_mesh)
-
             # create DTensor from Tensor
             _tensor = torch.empty(*input_size, device="cuda")
             dtensor = DTensor.from_local(_tensor, device_mesh, [Shard(1)])
@@ -262,6 +259,37 @@ class DistTensorRandomOpTest(DTensorTestBase):
                     self.assertEqual(local_tensor_gathered[slice_idx], local_tensor)
                 else:
                     self.assertNotEqual(local_tensor_gathered[slice_idx], local_tensor)
+
+
+    @with_comms
+    @skip_if_lt_x_gpu(4)
+    def test_megatron_init(self):
+        size = [8, 5]
+        mesh = torch.arange(self.world_size).reshape(2, 2)
+        device_mesh = DeviceMesh(self.device_type, mesh)
+        # weight sharding (row-wise parallel)
+        placements = [Shard(0), Replicate()]
+        data_parallel_group_1 = [0, 1]
+        # default seed is same within a MP group, and same within a DP group
+        # unless `data_parallel_random_init` is True.
+        default_seed = 100
+        # Model Parallel seed is different within a MP group
+        model_parallel_seed = 10 * (1 if self.rank in data_parallel_group_1 else 2)
+        model_parallel_seed += default_seed
+        # use MP seed to init
+        torch.cuda.manual_seed(model_parallel_seed)
+        local_tensor = torch.empty(size).uniform_()
+        dtensor_1 = DTensor.from_local(local_tensor, device_mesh, placements)
+        dtensor_1 = dtensor_1.redistribute(device_mesh, [Replicate(), Replicate()])
+
+        rng_tracker = MegatronCudaRNGStatesTracker()
+        # How to pass in DP/MP groups information?
+        rng_tracker.manual_seed(device_mesh, default_seed, model_parallel_seed)
+        local_tensor = torch.empty(size)
+        dtensor_2 = DTensor.from_local(local_tensor, device_mesh, placements)
+        dtensor_2.uniform_()
+        dtensor_2 = dtensor_2.redistribute(device_mesh, [Replicate(), Replicate()])
+        self.assertEqual(dtensor_1.to_local(), dtensor_2.to_local())
 
 
 if __name__ == "__main__":
--- torch/distributed/_tensor/random.py
+++ torch/distributed/_tensor/random.py
@@ -1,4 +1,5 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates
+import contextlib
 import math
 import warnings
 from typing import List
@@ -9,6 +10,7 @@ import torch.distributed as dist
 from torch import Tensor
 from torch.distributed._tensor.device_mesh import DeviceMesh
 from torch.distributed._tensor.placement_types import DTensorSpec, Shard
+from torch.distributed._tensor.rng_tracker import _rng_tracker
 
 
 def set_rng_state(new_state: Tensor, device_mesh: DeviceMesh) -> None:
@@ -65,48 +67,6 @@ def get_rng_state(device_mesh: DeviceMesh) -> Tensor:
         raise NotImplementedError(
             f"DTensor randomness only supports cuda device type, but got {device_mesh.device_type}"
         )
-
-
-def manual_seed(seed: int, device_mesh: DeviceMesh) -> None:
-    """Sets the seed for generating random numbers for the calling rank.
-
-    Args:
-        seed (int): The desired seed.
-        device_mesh (:class:`DeviceMesh`): The device mesh to set the seed.
-
-    Returns:
-        None
-
-    .. warning::
-        When calling this function, :func:`manual_seed` must be called from all ranks of the
-        default `ProcessGroup` even if some ranks may not be a part of the `device_mesh`,
-        with the same `seed` value.
-        If ``device_mesh`` is a sub-mesh and the calling rank is not a part of it,
-        `manual_seed` will not set its GPU device's generator seed.
-        Current implementation only supports a GPU device mesh.
-    """
-    assert isinstance(
-        device_mesh, DeviceMesh
-    ), f"expect a DeviceMesh but {type(device_mesh)} was passed in."
-
-    # allgather the seed from rank 0 over the default PG
-    object_list = [seed] * dist.get_world_size()
-    dist.all_gather_object(object_list, seed)
-    for rank, object in enumerate(object_list):
-        if seed != int(object):
-            raise RuntimeError(
-                f"calling manual_seed function over {device_mesh} but received different seed values on ranks:",
-                f"seed on rank {dist.get_rank()} is {seed}, and seed on rank {rank} is {object}!",
-            )
-
-    # the current rank is in mesh
-    if device_mesh.get_coordinate() is not None:
-        if device_mesh.device_type == "cuda":
-            torch.cuda.manual_seed(seed)
-        else:
-            raise NotImplementedError(
-                f"DTensor randomness only supports cuda device type, but got {device_mesh.device_type}"
-            )
 
 
 def set_pre_op_offset(spec: DTensorSpec) -> None:
@@ -301,3 +261,158 @@ def is_rng_supported_mesh(device_mesh: DeviceMesh) -> bool:
             f"DTensor random operators may not have complete support on {device_mesh.device_type} device mesh"
         )
         return False
+
+
+def manual_seed(seed: int, device_mesh: DeviceMesh, tp_dim: int=0) -> None:
+    """Sets the seed for generating random numbers for the calling rank.
+
+    Args:
+        seed (int): The desired seed.
+        device_mesh (:class:`DeviceMesh`): The device mesh to set the seed.
+        tp_dim (int, optional): The mesh dimension where to apply Tensor Parallel
+            Default: 0
+
+    Returns:
+        None
+
+    .. warning::
+        When calling this function, :func:`manual_seed` must be called from all ranks of the
+        default `ProcessGroup` even if some ranks may not be a part of the `device_mesh`,
+        with the same `seed` value.
+        If ``device_mesh`` is a sub-mesh and the calling rank is not a part of it,
+        `manual_seed` will not set its GPU device's generator seed.
+        Current implementation only supports a GPU device mesh.
+    """
+    # allgather the seed from rank 0 over the default PG
+    object_list = [seed] * dist.get_world_size()
+    dist.all_gather_object(object_list, seed)
+    for rank, object in enumerate(object_list):
+        if seed != int(object):
+            raise RuntimeError(
+                f"calling manual_seed function over {device_mesh} but received different seed values on ranks:",
+                f"seed on rank {dist.get_rank()} is {seed}, and seed on rank {rank} is {object}!",
+            )
+
+    # the current rank is in mesh
+    if device_mesh.get_coordinate() is not None:
+        if isinstance(_parallel_rng_style, MegatronRNG):
+            _parallel_rng_style._manual_seed(device_mesh, seed, tp_dim)
+        elif isinstance(_parallel_rng_style, DTensorRNG):
+            _parallel_rng_style._manual_seed(seed)
+        else:
+            raise RuntimeError("Unknown parallel RNG style!")
+
+
+# TODO: change this to ABC
+class ParallelRNGStyle:
+    def __init__(self):
+        pass
+
+    # override this function to define custom parallel random number generation
+    # to execute DTensor computation
+    @contextlib.contextmanager
+    def _parallel_region(self):
+        pass
+
+class DTensorRNG(ParallelRNGStyle):
+    # The default parallel random number generation style
+    def __init__(self):
+        super().__init__()
+
+    def _manual_seed(self, parallel_seed: int) -> None:
+        # do not pollute torch's default RNG state
+        device = torch.cuda.current_device()
+        with torch.random.fork_rng([device]):
+            torch.cuda.manual_seed(parallel_seed)
+            _rng_tracker.set_rng_state("parallel-rng", torch.cuda.get_rng_state())
+
+    @contextlib.contextmanager
+    def _parallel_region(self, spec: DTensorSpec):
+        # check if the parallel rng state has been synchronized or not
+        if not self.rng_state_is_sync("parallel-rng"):
+            raise RuntimeError(
+                "DTensorRNG requires random seed to be synchronized"
+                "before entering into a parallel region!"
+            )
+
+        # TODO: rewrite this set/restore logic using fork_rng()?
+        # fork rng state first since parallel RNG state should be used instead
+        old_state = torch.cuda.get_rng_state()  # a 16-byte ByteTensor
+        torch.cuda.set_rng_state(self.rng_states["parallel-rng"])
+        # set offset before entering the parallel region
+        mesh = spec.mesh
+        old_offset = _get_rng_offset(mesh)
+        set_pre_op_offset(spec)
+        try:
+            yield
+        finally:
+            # update offset to synchronize among ranks
+            set_post_op_offset(spec, old_offset)
+            _rng_tracker.set_rng_state("parallel-rng", torch.cuda.get_rng_state())
+            torch.cuda.set_rng_state(old_state)
+
+
+class MegatronRNG(ParallelRNGStyle):
+    def __init__(self):
+        super().__init__()
+
+    def _manual_seed(
+        self,
+        device_mesh: DeviceMesh,
+        default_seed: int = 1234,
+        tp_dim: int = 0,
+    ):
+        # Megatron maintains two sets of RNG states:
+        #   1. default state: torch's cuda RNG state. This is set to the same
+        #   across all ranks unless `data_parallel_random_init` is explicitly
+        #   set to True. Megatron uses this RNG state for non-tensor-parallel
+        #   regions.
+        #   2. tensor model parallel state: separate from the default state
+        #   and is only used in tensor-parallel regions. This is set to the same
+        #   across tensor parallel groups (i.e. along non-tp dimension) but different
+        #   within a tensor parallel group.
+
+        # calculate tensor_model_parallel_seed as Megatron does
+        # source: https://github.com/NVIDIA/Megatron-LM/blob/main/megatron/core/tensor_parallel/random.py
+        tensor_model_parallel_rank = device_mesh.get_coordinate()[tp_dim]
+        tensor_model_parallel_seed = default_seed + 2718 + tensor_model_parallel_rank
+
+        # store tensor model parallel RNG state
+        torch.cuda.manual_seed(tensor_model_parallel_seed)
+        _rng_tracker.set_rng_state("model-parallel-rng", torch.cuda.get_rng_state())
+
+        # set torch default RNG state with the default seed
+        torch.cuda.manual_seed(default_seed)
+
+    @contextlib.contextmanager
+    def _parallel_region(self, spec: DTensorSpec):
+        # check if the parallel rng state has been synchronized or not
+        if not self.rng_state_is_sync("parallel-rng"):
+            raise RuntimeError(
+                "MegatronRNG requires random seed to be synchronized"
+                "before entering into a parallel region!"
+            )
+        
+        if not self.rng_state_is_sync("model-parallel-rng"):
+            default_seed = torch.cuda.initial_seed()
+            mesh = spec.mesh
+            # this mesh should be a 1-D mesh representing a
+            # Tensor Parallel group
+            tensor_model_parallel_rank = mesh.get_coordinate()[0]
+            tensor_model_parallel_seed = default_seed + 2718 + tensor_model_parallel_rank
+            device = torch.cuda.current_device()
+            with torch.random.fork_rng([device]):
+                torch.cuda.manual_seed(tensor_model_parallel_seed)
+                _rng_tracker.set_rng_state("model-parallel-rng", torch.cuda.get_rng_state())
+
+        # fork rng state first since tensor model parallel RNG state should be used instead
+        old_state = torch.cuda.get_rng_state()  # a 16-byte ByteTensor
+        torch.cuda.set_rng_state(self.rng_states["model-parallel-rng"])
+        try:
+            yield
+        finally:
+            _rng_tracker.set_rng_state("model-parallel-rng", torch.cuda.get_rng_state())
+            torch.cuda.set_rng_state(old_state)
+
+
+_parallel_rng_style = DTensorRNG()
