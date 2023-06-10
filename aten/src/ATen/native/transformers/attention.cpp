5fcd2829a88363d34ee34c9ae6474699ddcafbe6
Michael Gschwind <61328285+mikekgfb@users.noreply.github.com>
2023-06-10 12:30:05 +0800

    Update attention.cpp to remove warning about preferring torch.bool data type
    Update attention.cpp to remove warning about preferring torch.bool data type

--- aten/src/ATen/native/transformers/attention.cpp
+++ aten/src/ATen/native/transformers/attention.cpp
@@ -146,12 +146,6 @@ Tensor masked_softmax(
   if (query.is_nested() && !attn_mask) {
     return at::_nested_tensor_softmax_with_shape(attn_scores, query);
   }
-  if (attn_mask && attn_mask->dtype() != at::kBool) {
-    TORCH_WARN(
-        "Converting mask without torch.bool dtype to bool; this will "
-        "negatively affect performance. Prefer to use a boolean mask directly.");
-    attn_mask = attn_mask->to(at::kBool);
-  }
   if (attn_mask) {
     return _masked_softmax(attn_scores, *attn_mask, attn_scores.dim() - 1, mask_type);
   } else {
